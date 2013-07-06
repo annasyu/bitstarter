98a22e640a9e580c4b7afe932ba067ded65c3519
Anna S. Yu <anna.s.yu@gmail.com>
2013-07-06 20:23:07 +0000

    fix sync bug

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buf = new Buffer(8);
-  response.send(buf.toString(fs.readFileSync("index.html")));
+  response.send(fs.readFileSync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
