950777b1215e02406dd424ff01bf71e379e47414
Anna S. Yu <anna.s.yu@gmail.com>
2013-07-06 20:26:40 +0000

    fix buffer bug

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer(8);
-  response.send(fs.readFileSync("index.html"));
+  response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
